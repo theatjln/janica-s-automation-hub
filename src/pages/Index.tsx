ea25ed791efbae2304eb7db4b651e34c6dc50369
jl <janicaliz.dev@gmail.com>
2026-01-01 14:05:44 +0000

    put the projects first before work experience

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,8 +14,8 @@ const Index = () => {
       <main>
         <Hero />
         <Services />
-        {/* <Experience /> hide for now */}
         <Projects />
+        {/* <Experience /> hide for now */}
         <Testimonials />
         <Contact />
       </main>
